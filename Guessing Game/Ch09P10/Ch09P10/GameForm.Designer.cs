2c1f9c65e8cb46b64fde3571cc6aa594465fef69
crewszk <56327393+crewszk@users.noreply.github.com>
2019-10-08 21:39:16 -0400

    Add files via upload

---
+++ Guessing Game/Ch09P10/Ch09P10/GameForm.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Ch09P10
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBxGuess = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBxNumberOfGuesses = new System.Windows.Forms.TextBox();
+            this.txtBxVariation = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.giveUpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.giveUpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(153, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(305, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Zack\'s Wonderful Guessing Game!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Your Guess:";
+            // 
+            // txtBxGuess
+            // 
+            this.txtBxGuess.Location = new System.Drawing.Point(165, 105);
+            this.txtBxGuess.Name = "txtBxGuess";
+            this.txtBxGuess.Size = new System.Drawing.Size(140, 28);
+            this.txtBxGuess.TabIndex = 2;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(116, 151);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(95, 39);
+            this.btnGuess.TabIndex = 3;
+            this.btnGuess.Text = "Guess?";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(328, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "# of Guesses:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(328, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Variation From #:";
+            // 
+            // txtBxNumberOfGuesses
+            // 
+            this.txtBxNumberOfGuesses.Location = new System.Drawing.Point(521, 105);
+            this.txtBxNumberOfGuesses.Name = "txtBxNumberOfGuesses";
+            this.txtBxNumberOfGuesses.Size = new System.Drawing.Size(42, 28);
+            this.txtBxNumberOfGuesses.TabIndex = 6;
+            // 
+            // txtBxVariation
+            // 
+            this.txtBxVariation.Location = new System.Drawing.Point(521, 148);
+            this.txtBxVariation.Name = "txtBxVariation";
+            this.txtBxVariation.Size = new System.Drawing.Size(42, 28);
+            this.txtBxVariation.TabIndex = 7;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.giveUpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(610, 28);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // giveUpToolStripMenuItem
+            // 
+            this.giveUpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.giveUpToolStripMenuItem1,
+            this.restartToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.giveUpToolStripMenuItem.Name = "giveUpToolStripMenuItem";
+            this.giveUpToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.giveUpToolStripMenuItem.Text = "Options";
+            // 
+            // giveUpToolStripMenuItem1
+            // 
+            this.giveUpToolStripMenuItem1.Name = "giveUpToolStripMenuItem1";
+            this.giveUpToolStripMenuItem1.Size = new System.Drawing.Size(181, 26);
+            this.giveUpToolStripMenuItem1.Text = "Give Up?";
+            this.giveUpToolStripMenuItem1.Click += new System.EventHandler(this.giveUpToolStripMenuItem1_Click);
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.restartToolStripMenuItem.Text = "Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(211, 209);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(0, 20);
+            this.lblInfo.TabIndex = 9;
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(610, 253);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.txtBxVariation);
+            this.Controls.Add(this.txtBxNumberOfGuesses);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.txtBxGuess);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Verdana", 10F);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "GameForm";
+            this.Text = "Zack\'s Guessing Game";
+            this.Load += new System.EventHandler(this.GameForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBxGuess;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBxNumberOfGuesses;
+        private System.Windows.Forms.TextBox txtBxVariation;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem giveUpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem giveUpToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ Guessing Game/Ch09P10/Ch09P10/GameForm.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ch09P10
+{
+    public partial class GameForm : Form
+    {
+        Random n = new Random();
+        private int randomNumber;
+        private int counter;
+
+        public GameForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            int input;
+
+            txtBxGuess.Focus();
+
+            if (int.TryParse(txtBxGuess.Text, out input) == false | input < 1 | input > 50)
+            {
+                if (txtBxGuess.Text == "")
+                    MessageBox.Show("Please guess a number 1 - 50 and enter it into the appropraite box labeled \"Your Guess\".",
+                                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                    MessageBox.Show("Please only enter positive numeric values between 1 - 50.", "Invalid Input",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+
+                if (input < randomNumber)
+                {
+                    counter++;
+                    txtBxNumberOfGuesses.Text = string.Concat(counter);
+                    txtBxVariation.Text = string.Concat(input);
+                    txtBxVariation.BackColor = Color.Red;
+                    txtBxVariation.ForeColor = Color.White;
+                    lblInfo.Text = "Too Low! Try again!";
+                    
+                }
+                else if (input > randomNumber)
+                {
+                    counter++;
+                    txtBxNumberOfGuesses.Text = string.Concat(counter);
+                    txtBxVariation.Text = string.Concat(input);
+                    txtBxVariation.BackColor = Color.Blue;
+                    txtBxVariation.ForeColor = Color.White;
+                    lblInfo.Text = "Too High! Try again!";
+                    
+                }
+                else if (input == randomNumber)
+                {
+                    counter++;
+                    txtBxNumberOfGuesses.Text = string.Concat(counter);
+                    txtBxVariation.Text = string.Concat(input);
+                    txtBxVariation.BackColor = Color.Green;
+                    txtBxVariation.ForeColor = Color.White;
+                    lblInfo.Text = "SPOT ON! CONGRATULATIONS!";
+                    
+                    AskForRestart();
+                }
+            }
+
+        }
+
+        private void GameForm_Load(object sender, EventArgs e)
+        {
+            string loadText = "This is a guessing game application. Your objective is to try and guess\n" +
+                              "a number 1 - 50 that the application has picked randomly. The application\n" +
+                              "will log how many guesses it takes you, if your guess was too high, and if\n" +
+                              "your guess was too low. Are you ready to begin?";
+
+            MessageBox.Show(loadText, "Welcome!", MessageBoxButtons.OK);
+
+            lblInfo.Text = "";
+            txtBxGuess.Focus();
+            txtBxGuess.Clear();
+            txtBxNumberOfGuesses.Clear();
+            txtBxVariation.Clear();
+            txtBxNumberOfGuesses.ReadOnly = true;
+            txtBxVariation.ReadOnly = true;
+            txtBxNumberOfGuesses.BackColor = Color.White;
+            txtBxVariation.BackColor = Color.White;
+            txtBxGuess.BackColor = Color.White;
+            counter = 0;
+            randomNumber = n.Next(0, 51);
+        }
+
+        public void AskForRestart()
+        {
+            string result = "Congratulations on guessing the number correctly!\n" +
+                            "\nCorrect Number: " + randomNumber + "\nNumber of Guesses: " +
+                            counter + "\n\nWould you like to play again?";
+
+
+            if (MessageBox.Show(result, "Congratulations!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                RestartApplication();
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+        public void RestartApplication()
+        {
+            string restartText = "You have chosen to restart the game! A new random\n" +
+                                 "number has been selected from 1 - 50";
+
+            MessageBox.Show(restartText, "Loading...", MessageBoxButtons.OK);
+            lblInfo.Text = "";
+            txtBxGuess.Focus();
+            txtBxGuess.Clear();
+            txtBxNumberOfGuesses.Clear();
+            txtBxVariation.Clear();
+            txtBxNumberOfGuesses.ReadOnly = true;
+            txtBxVariation.ReadOnly = true;
+            txtBxNumberOfGuesses.BackColor = Color.White;
+            txtBxVariation.BackColor = Color.White;
+            txtBxGuess.BackColor = Color.White;
+            counter = 0;
+            randomNumber = n.Next(0, 51);
+        }
+
+        private void giveUpToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            string result = "Its a shame you couldn't guess the correct number :(" +
+                            "\nBefore you quit you guessed " + counter + " times." +
+                            "\n\nThe correct number was " + randomNumber + 
+                            "\n\nDo you want to try again?";
+
+            if (MessageBox.Show("Are you sure you want to give up?", "Well are you?", 
+                                MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
+            {
+                if (MessageBox.Show(result, "Too Bad :(", MessageBoxButtons.YesNo, 
+                                    MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    RestartApplication();
+                }
+                else
+                {
+                    Application.Exit();
+                }
+            }
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to completely restart?\nThe randomized number will not be shown.",
+                                "Restart?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                RestartApplication();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to exit the application?", "Well do you?", 
+                                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
